b5d9fc8bcd0ff7a2c9d59a8e1c29f79bb6d7bc41
MOHIT SINGH RAJPUT <mohitsinghdausa@gmail.com>
2026-06-21 09:14:50 +0530

    Time: 75 ms (78.45%) | Memory: 31 MB (81.86%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        max_cost = max(costs)
+
+        freq = [0] * (max_cost + 1)
+
+        for cost in costs:
+            freq[cost] += 1
+
+        bars = 0
+
+        for price in range(1, max_cost + 1):
+            if freq[price] == 0:
+                continue
+
+            can_buy = min(freq[price], coins // price)
+
+            bars += can_buy
+            coins -= can_buy * price
+
+            if coins < price:
+                continue
+
+        return bars
