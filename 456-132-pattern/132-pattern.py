0060d2bc749644b2be07321447577714ab048f68
MOHIT SINGH RAJPUT <mohitsinghdausa@gmail.com>
2026-07-13 13:14:41 +0530

    Time: 35 ms (99.52%) | Memory: 36.3 MB (68.79%) - LeetSync

---
+++ 456-132-pattern/132-pattern.py
@@ -0,0 +1,16 @@
+class Solution:
+    def find132pattern(self, nums: list[int]) -> bool:
+        stack = []
+        third = float('-inf')
+
+        for num in reversed(nums):
+
+            if num < third:
+                return True
+
+            while stack and num > stack[-1]:
+                third = stack.pop()
+
+            stack.append(num)
+
+        return False
