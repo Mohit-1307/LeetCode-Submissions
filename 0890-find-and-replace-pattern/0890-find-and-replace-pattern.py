1bc4b1e361953c74a4388e5acdf2f09b0475d83a
MOHIT SINGH RAJPUT <mohitsinghdausa@gmail.com>
2026-08-03 09:41:36 +0530

    Time: 0 ms (100%), Space: 19.4 MB (36.26%) - LeetHub

---
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        def match(word):
+            p_to_w = {}
+            w_to_p = {}
+
+            for p, w in zip(pattern, word):
+                if p in p_to_w:
+                    if p_to_w[p] != w:
+                        return False
+                else:
+                    p_to_w[p] = w
+
+                if w in w_to_p:
+                    if w_to_p[w] != p:
+                        return False
+                else:
+                    w_to_p[w] = p
+
+            return True
+
+        return [word for word in words if match(word)]
