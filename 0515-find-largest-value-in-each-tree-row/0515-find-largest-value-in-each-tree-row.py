3f9fbff2bb0694e53abb6075196dae09260be182
MOHIT SINGH RAJPUT <mohitsinghdausa@gmail.com>
2026-08-22 15:31:27 +0530

    Time: 2 ms (48.19%), Space: 21 MB (53.77%) - LeetHub

---
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Solution:
+    def largestValues(self, root):
+        if not root:
+            return []
+
+        queue = deque([root])
+        ans = []
+
+        while queue:
+            level_max = float('-inf')
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+
+                level_max = max(level_max, node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            ans.append(level_max)
+
+        return ans
